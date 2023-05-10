87cf459d21620778ec52a86668263ff769743bbc
moezabdelkef <moezabdelkefi17@gmail.com>
2023-05-10 13:34:42 +0100

    From Development to Production

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 # Install Hugo and Make
-apt-get update && apt-get install -y hugo make
+apt-get update
+apt-get install -y hugo make
 
-# Clone your project repository
-git clone https://github.com/moezabdelkefi/holbertonschool-validation
-
-# Change into the project directory
-cd module1_task3
-
-# Build the website using Make
+# Build the website
 make build
